61b0e07f9c77e487e7e769096f6bc40fe3a0ea72
shxnvrd <trickmen47@gmail.com>
2025-07-14 12:04:04 +0500

    Работа над рисованием: * Исправлена ошибка при которой не происходило изменение символа рисования * Добавлены функции копирования, вставки и вырезания выделенных символов * Добавлено контекстное меню для доступа к функциям копирования, вставки и вырезания Рефакторинг: * Данные о выделении вынесены в класс Selection

--- src/Strasciierry.UI/Controls/AsciiCanvas/AsciiCanvas.xaml.cs
+++ src/Strasciierry.UI/Controls/AsciiCanvas/AsciiCanvas.xaml.cs
@@ -1,13 +1,16 @@
 using System.Collections.ObjectModel;
-using Microsoft.UI;
+using System.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Shapes;
 using Strasciierry.UI.Controls.AsciiCanvas.ToolHandlers;
 using Strasciierry.UI.Controls.AsciiCanvas.ToolHandlers.Base;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
+using Windows.UI;
 
 namespace Strasciierry.UI.Controls.AsciiCanvas;
 
@@ -31,7 +34,7 @@ public sealed partial class AsciiCanvas : UserControl
         get => (char)GetValue(DrawingCharProperty);
         set
         {
-            if (DrawingChar != value)
+            if (DrawingChar == value)
                 return;
             
             SetValue(DrawingCharProperty, value);
@@ -87,11 +90,14 @@ public sealed partial class AsciiCanvas : UserControl
     public ObservableCollection<CharCell> Cells { get; }
            = new ObservableCollection<CharCell>();
 
-    private Rectangle? _selectionRect;
-    private Point _selectionStartPoint;
+    public double CellWidth => CanvasRepeater.ActualWidth / Columns;
+    public double CellHeight => CanvasRepeater.ActualHeight / Rows;
+
+    private Selection? _selection;
     private Point _lastCellPosition = new(-1, -1);
     private readonly ReadOnlyDictionary<DrawingTool, ToolHandler> _toolHandlers;
     private ToolHandler _currentToolHandler;
+    private const char DefaultCharacter = ' ';
 
     public AsciiCanvas()
     {
@@ -120,9 +126,9 @@ public sealed partial class AsciiCanvas : UserControl
 
         Cells.Clear();
 
-        for (int row = 0; row < Rows; row++)
+        for (var row = 0; row < Rows; row++)
         {
-            for (int col = 0; col < Columns; col++)
+            for (var col = 0; col < Columns; col++)
             {
                 Cells.Add(new CharCell(col, row));
             }
@@ -183,65 +189,214 @@ public sealed partial class AsciiCanvas : UserControl
         }
     }
 
-    public void UpdateSelection(int col, int row)
+    public void UpdateSelection(int column, int row)
     {
-        if (_selectionRect == null)
-        {
-            _selectionStartPoint = new Point(col, row);
+        if (!IsValidPoint(column, row))
+            throw new ArgumentOutOfRangeException($"Columns and rows must be greater than or equal to zero and less than total columns: {Columns} and rows: {Rows}");
 
-            _selectionRect = new Rectangle
+        if (_selection is null)
+        {
+            _selection = new Selection
             {
-                Fill = new SolidColorBrush(Colors.Blue)
+                Area = new Rectangle
                 {
-                    Opacity = 0.3
+                    Fill = new SolidColorBrush
+                    { 
+                        Color = Color.FromArgb(50, 0, 0, 0),
+                        Opacity = 0.3
+                    },
+                    IsHitTestVisible = false
                 },
-                IsHitTestVisible = false
+                InitialColumn = column,
+                InitialRow = row,
+                StartRow = column,
+                StartColumn = row
             };
 
-            SelectionLayer.Children.Add(_selectionRect);
+            SelectionLayer.Children.Add(_selection.Area);
         }
 
-        var startCol = (int)_selectionStartPoint.X;
-        var startRow = (int)_selectionStartPoint.Y;
+        int minColumn = Math.Min(_selection.InitialColumn, column);
+        int maxColumn = Math.Max(_selection.InitialColumn, column);
+        int minRow = Math.Min(_selection.InitialRow, row);
+        int maxRow = Math.Max(_selection.InitialRow, row);
 
-        int minCol = Math.Min(startCol, col);
-        int maxCol = Math.Max(startCol, col);
-        int minRow = Math.Min(startRow, row);
-        int maxRow = Math.Max(startRow, row);
+        double left = minColumn * CellWidth;
+        double top = minRow * CellHeight;
+        double width = (maxColumn - minColumn + 1) * CellWidth;
+        double height = (maxRow - minRow + 1) * CellHeight;
 
-        double cellWidth = CanvasRepeater.ActualWidth / Columns;
-        double cellHeight = CanvasRepeater.ActualHeight / Rows;
+        _selection.StartColumn = minColumn;
+        _selection.StartRow = minRow;
 
-        double left = minCol * cellWidth;
-        double top = minRow * cellHeight;
-        double width = (maxCol - minCol + 1) * cellWidth;
-        double height = (maxRow - minRow + 1) * cellHeight;
-
-        Canvas.SetLeft(_selectionRect, left);
-        Canvas.SetTop(_selectionRect, top);
-        _selectionRect.Width = width;
-        _selectionRect.Height = height;
+        Canvas.SetLeft(_selection.Area, left);
+        Canvas.SetTop(_selection.Area, top);
+        _selection.Area.Width = width;
+        _selection.Area.Height = height;
     }
 
     public void ClearSelection()
     {
         SelectionLayer.Children.Clear();
-        _selectionRect = null;
+        _selection = null;
     }
 
-    private CharCell? GetCell(Point position)
+    private CharCell GetCell(int column, int row)
     {
-        double cellWidth = CanvasRepeater.ActualWidth / Columns;
-        double cellHeight = CanvasRepeater.ActualHeight / Rows;
+        if (!IsValidPoint(column, row))
+            throw new ArgumentOutOfRangeException($"Columns and rows must be greater than or equal to zero and less than total columns: {Columns} and rows: {Rows}");
 
-        var col = (int)(position.X / cellWidth);
-        var row = (int)(position.Y / cellHeight);
+        return Cells[row * Columns + column];
+    }
 
-        if (col >= 0 && col < Columns && row >= 0 && row < Rows)
+    private void CanvasRepeater_RightTapped(object sender, RightTappedRoutedEventArgs e)
+    {
+        var element = (FrameworkElement)sender;
+
+        var flyout = FlyoutBase.GetAttachedFlyout(element);
+        var options = new FlyoutShowOptions
         {
-            return Cells[row * Columns + col];
+            Placement = FlyoutPlacementMode.RightEdgeAlignedBottom,
+            ShowMode = FlyoutShowMode.Standard,
+            Position = e.GetPosition(element)
+        };
+
+        flyout?.ShowAt(element, options);
+
+    }
+
+    private void OnCopyClick(object sender, RoutedEventArgs e)
+    {
+        if (_selection is null)
+            return;
+
+        var selectionEndColumn = (int)(_selection.Area.Width / CellWidth + _selection.StartColumn) - 1;
+        var selectionEndRow = (int)(_selection.Area.Height / CellHeight + _selection.StartRow) - 1;
+
+        var text = GetCharacters(_selection.StartColumn, _selection.StartRow, selectionEndColumn, selectionEndRow);
+
+        var package = new DataPackage
+        {
+            RequestedOperation = DataPackageOperation.Copy
+        };
+        package.SetText(text);
+        Clipboard.SetContent(package);
+    }
+
+    private async void OnPasteClick(object sender, RoutedEventArgs e)
+    {
+        if (_selection is null)
+            return;
+
+        var package = Clipboard.GetContent();
+        if (!package.Contains(StandardDataFormats.Text))
+            return;
+
+        var pastingText = await package.GetTextAsync();
+        if (pastingText is null)
+            return;
+
+        var selectedColumnsCount = (int)(_selection.Area.Width / CellWidth);
+        var selectedRowsCount = (int)(_selection.Area.Height / CellHeight);
+
+        PasteCharacters(_selection.StartColumn, _selection.StartRow, selectedColumnsCount, selectedRowsCount, pastingText);       
+    }
+
+    private void OnCutClick(object sender, RoutedEventArgs e)
+    {
+        if (_selection is null)
+            return;
+
+        var selectionEndColumn = (int)(_selection.Area.Width / CellWidth + _selection.StartColumn) - 1;
+        var selectionEndRow = (int)(_selection.Area.Height / CellHeight + _selection.StartRow) - 1;
+
+        var text = CutCharacters(_selection.StartColumn, _selection.StartRow, selectionEndColumn, selectionEndRow);
+
+        var package = new DataPackage
+        {
+            RequestedOperation = DataPackageOperation.Move
+        };
+        package.SetText(text);
+        Clipboard.SetContent(package);
+    }
+
+    private void PasteCharacters(int startColumn, int startRow, int columnsCount, int rowsCount, string text)
+    {
+        if (text is null)
+            return;
+
+        var lines = text.Split("\r\n");
+
+        var totalRows = Math.Min(lines.Length, rowsCount);
+
+        for (var rowOffset = 0; rowOffset < totalRows; rowOffset++)
+        {
+            var line = lines[rowOffset];
+            var totalColumns = Math.Min(line.Length, columnsCount);
+
+            for (var colOffset = 0; colOffset < totalColumns; colOffset++)
+            {
+                var col = startColumn + colOffset;
+                var row = startRow + rowOffset;
+
+                var cell = GetCell(col, row);
+                if (cell is not null)
+                {
+                    cell.Character = line[colOffset];
+                }
+            }
+        }
+    }
+
+    private string GetCharacters(int startColumn, int startRow, int endColumn, int endRow)
+    {
+        if (!(IsValidPoint(startColumn, startRow)
+            && IsValidPoint(endColumn, endRow)))
+        {
+            throw new ArgumentOutOfRangeException($"Columns and rows must be greater than or equal to zero and less than total columns: {Columns} and rows: {Rows}");
         }
 
-        return null;
+        var sb = new StringBuilder();
+
+        for (var row = startRow; row <= endRow; row++)
+        {
+            for (var column = startColumn; column <= endColumn; column++)
+            {
+                var cell = GetCell(column, row);
+                sb.Append(cell.Character);
+            }
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
     }
+
+    private string CutCharacters(int startColumn, int startRow, int endColumn, int endRow)
+    {
+        if (!(IsValidPoint(startColumn, startRow)
+            && IsValidPoint(endColumn, endRow)))
+        {
+            throw new ArgumentOutOfRangeException($"Columns and rows must be greater than or equal to zero and less than total columns: {Columns} and rows: {Rows}");
+        }
+
+        var sb = new StringBuilder();
+
+        for (var row = startRow; row <= endRow; row++)
+        {
+            for (var column = startColumn; column <= endColumn; column++)
+            {
+                var cell = GetCell(column, row);
+                sb.Append(cell.Character);
+                cell.Character = DefaultCharacter;
+            }
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+
+    private bool IsValidPoint(int column, int row)
+        => column >= 0 && column < Columns
+           && row >= 0 && row < Rows;
+
 }
---
+++ src/Strasciierry.UI/Controls/AsciiCanvas/Selection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Shapes;
+
+namespace Strasciierry.UI.Controls.AsciiCanvas;
+
+class Selection
+{
+    public Rectangle Area { get; set; }
+    public int InitialRow { get; set; }
+    public int InitialColumn { get; set; }
+    public int StartRow { get; set; }
+    public int StartColumn { get; set; }
+}
